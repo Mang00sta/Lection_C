35d5a3cdb9aeb5411c0ef84b5c1f54c7da102c5a
Vishnyakov <mangostalife@gmail.com>
2022-06-26 18:01:54 +0300

    10 задача закончена

--- lecture_1/Example/Exa010_Metod/Program.cs
+++ lecture_1/Example/Exa010_Metod/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int[] array = {1, 12, 31, 4, 15, 16, 17, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index=0;
+
+while (index < n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break; // прирывает алгоритм
+    }
+    
+    index++;
+}
