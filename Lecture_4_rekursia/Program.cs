46ad43cbd20608ccdcbe157a082b4939e4ea6d8e
Vishnyakov <mangostalife@gmail.com>
2022-07-11 20:02:35 +0300

    Лекция половинка

---
+++ Lecture_4_rekursia/Program.cs
@@ -0,0 +1,119 @@
+﻿/*
+string[,] table = new string[2, 5];
+//string.Empty
+// table[0,0] table[0,1] table[0,2] table[0,3] table[0,4] table[0,5] - столбцы
+// table[1,0] table[1,1] table[1,2] table[1,3] table[1,4] table[1,5] - столбцы
+table[1, 2] = "слово";
+
+for(int rows=0; rows<2; rows++) // строки
+{
+    for (int columns=0; columns<5; columns++) // столбцы 
+    {
+        Console.WriteLine($"-{table[rows, columns]}-"); // 
+    }
+}
+/*/
+/*
+int[,] matrix = new int [3,4];
+
+for(int i=0; i<3; i++)// строки
+{
+    for (int j=0; j<4; j++) // столбцы 
+    {
+        Console.Write($"{matrix[i,j]} "); // 
+    }
+Console.WriteLine();
+}
+/*/
+
+
+/*
+void PrintArray(int[,] matr)
+{
+    for(int i=0; i<matr.GetLength(0); i++)// <matrix.GetLength(0) 
+    {
+        for (int j=0; j<matr.GetLength(1); j++) // <matrix.GetLength(1) 
+        {
+            Console.Write($"{matr[i,j]} "); // 
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillRay (int[,] matr)
+{
+    for(int i=0; i<matr.GetLength(0); i++)// <matrix.GetLength(0) 
+    {
+        for (int j=0; j<matr.GetLength(1); j++) // <matrix.GetLength(1) 
+        {
+            matr[i,j] = new Random().Next(1,10); // 1;10
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int [3,4];
+PrintArray(matrix);
+FillRay (matrix);
+Console.WriteLine();
+PrintArray(matrix);
+/*/
+
+int[,] pic = new int[,]
+{
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+ {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+
+
+void PrintImage(int[,] image)
+{
+    for(int i=0; i<image.GetLength(0); i++)// <matrix.GetLength(0) 
+    {
+        for (int j=0; j<image.GetLength(1); j++) // <matrix.GetLength(1) 
+        {
+           if (image[i,j]==0) Console.Write($" ");
+           else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillImage (int row, int col)
+{
+    if (pic[row,col]==0)
+    {
+        pic[row,col]=1;
+        FillImage (row-1,col);
+        FillImage (row,col-1);
+        FillImage (row+1,col);
+        FillImage (row,col+1);
+        
+    }
+    
+}
+PrintImage(pic);
+FillImage(13,13);
+PrintImage(pic);
+
